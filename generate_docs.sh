578cda439b2430267fc3e378a9e30ad42ff827cb
lata <imlata1111@gmail.com>
2024-04-15 16:25:38 +0530

    Add script for generating RM GSMA docs

---
+++ generate_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#can test this by running command ./generate_docs.sh
+
+
+echo "Generating RM GSMA docs..."
+# Replace the above command with the actual command to generate the docs
+
+echo "RM GSMA docs generated successfully."
